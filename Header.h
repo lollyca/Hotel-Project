1bfec6424e488b43d7bfacc6fd5a3e58737027f3
Heloisa Santos <heloisacmk@hotmail.com>
2022-10-25 10:37:19 -0700

    fiz 4 arrays, mas vou tentar fazer 1 grande array

--- Header.h
+++ Header.h
@@ -1,24 +1,37 @@
-#pragma once
+
 #ifndef HEADER_H
 #define HEADER_H
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <sstream>
+#include <array>
+
 
 using namespace std;
 
-class Hotel {
-
+class Room {
 private:
-	int numberOfRooms, roomPrice, roomsReserved=0, roomsAvailable=0, income=0;
-	string roomName;
+	int roomNumber;
+	double roomPrice;
+	string roomStyle;
+	bool isAvailable;
+	
 
 public:
-	Hotel(int rooms, int price, string name);
-	void displayInventory();
-	void roomReservation(int numReserved);
-	void roomRevenue();
+	void setName(Room array[], int size, string name);
+	void setNumber(Room array[], int size, int roomNumber);
+	void setPrice(Room array[], int size, double price);
+	void setCondition(Room array[], int size);
+	int getRoomNumber() {return roomNumber;}
+	double getRoomPrice() {return roomPrice;}
+	string getRoomStyle() {return roomStyle;}
+	bool getCondition() { return isAvailable; }
+
+	void setReservation(Room array[]);
+
+
 };
+
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,30 +1,74 @@
 #include "Header.h"
+using namespace std;
 
 int main()
 {
-    Hotel courtyard(70, 125, "Courtyard");
-    Hotel scenic(35, 145, "Scenic");
-    Hotel suite(15, 350, "Suite");
-    Hotel penthouse(2, 1135, "Penthouse");
+	int totalRooms = 122;
+	const int COUT = 70;
+	const int SCENIC = 35;
+	const int SUITE = 15;
+	const int PENT = 2;
+	int date, reservationInput;
 
-    cout << setw(25) << "HOTEL INVENTORY" << endl;
-    cout << setw(15) << left << "ROOM STYLE" << setw(2) << "PRICE" << setw(10) << right << "ROOMS" << endl;
-    courtyard.displayInventory();
-    scenic.displayInventory();
-    suite.displayInventory();
-    penthouse.displayInventory();
-    cout << "\n";
-    courtyard.roomReservation(4);
-    cout << "\n";
-    courtyard.roomRevenue(); 
-    cout << "\n";
-    courtyard.roomReservation(1);
-    cout << "\n";
-    courtyard.roomRevenue();
-    cout << "\n";
-    suite.roomReservation(2);
-    cout << "\n";
-    suite.roomRevenue();
+
+	Room arrCourtyard[COUT];
+	Room arrScenic[SCENIC];
+	Room arrSuite[SUITE];
+	Room arrPenthouse[PENT];
+
+
+	cout << "Enter today's date ---> ";
+	cin >> date;
+
+	arrCourtyard->setName(arrCourtyard, COUT, "Courtyard");
+	arrScenic->setName(arrScenic, SCENIC, "Scenic");
+	arrSuite->setName(arrSuite, SUITE, "Deluxe Suite");
+	arrPenthouse->setName(arrPenthouse, PENT, "Penthouse");
+
+	arrCourtyard->setPrice(arrCourtyard, COUT, 125);
+	arrScenic->setPrice(arrScenic, SCENIC, 145);
+	arrSuite->setPrice(arrSuite, SUITE, 350);
+	arrPenthouse->setPrice(arrPenthouse, PENT, 1135);
+
+	arrCourtyard->setCondition(arrCourtyard, COUT);
+	arrScenic->setCondition(arrScenic, SCENIC);
+	arrSuite->setCondition(arrSuite, SUITE);
+	arrPenthouse->setCondition(arrPenthouse, PENT);
+
+
+	cout << "Inventory" << endl;
+	cout << "Courtyard - " << COUT << " - $" << arrCourtyard->getRoomPrice() << " a night" << endl;
+	cout << "Scenic - " << SCENIC << " - $" << arrScenic->getRoomPrice() << " a night" << endl;
+	cout << "Deluxe Suite - " << SUITE << " - $" << arrSuite->getRoomPrice() << " a night" << endl;
+	cout << "Penthouse - " << PENT << " - $" << arrPenthouse->getRoomPrice() << " a night" << endl;
+	cout << "\n";
+
+	cout << "Reservation system: " << endl;
+	cout << "What room would you like to reserve? " << endl;
+	cout << "1 --- Courtyard - 70 - $125 a night" << endl;
+	cout << "2 --- Scenic - 35 - $145 a night" << endl;
+	cout << "3 --- Deluxe Suite - 15 - $350 a night" << endl;
+	cout << "4 --- Penthouse - 2 - $1135 a night" << endl;
+	cin >> reservationInput;
+
+	switch (reservationInput)
+	{
+	case 1:
+		arrCourtyard->setReservation(arrCourtyard);
+		break;
+	case 2:
+		arrScenic->setReservation(arrScenic);
+		break;
+	case 3:
+		arrSuite->setReservation(arrSuite);
+		break;
+	case 4:
+		arrPenthouse->setReservation(arrPenthouse);
+		break;
+	}
+
 }
+
+
 
 
